ad0b2c796e4e189e57db8f955c1f990954e92b14
SeaOceanLiu <seaocean.liu@163.com>
2025-08-14 13:01:21 +0800

    1、修正DestroyDebugForm命名 2、设置源码文件格式为UTF-8 with BOM 3、Tab替换为空格 4、使用初始化列表来初始化

--- DebugTrace.h
+++ DebugTrace.h
@@ -1,4 +1,4 @@
-//---------------------------------------------------------------------------
+﻿//---------------------------------------------------------------------------
 
 #ifndef DebugTraceH
 #define DebugTraceH
@@ -10,17 +10,17 @@
 //#include "..\debug\debugformunit.h"
 #define MAX_SYS_ERROR_MSG_LEN 500
 #define PRINT_OUT_LAST_ERROR { char ErrorInfo[MAX_SYS_ERROR_MSG_LEN];           \
-                               LPVOID   lpMsgBuf;                               \
-                               FormatMessage(                                   \
-                                   FORMAT_MESSAGE_ALLOCATE_BUFFER               \
-                                       | FORMAT_MESSAGE_FROM_SYSTEM,            \
-                                   NULL, GetLastError(),                        \
-                                   MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),   \
-                                   (LPTSTR) &lpMsgBuf, 0, NULL);                \
-                               sprintf_s(ErrorInfo, MAX_SYS_ERROR_MSG_LEN, "Error at File:%s, Line:%d, %s",__FILE__, __LINE__, (char *)lpMsgBuf);\
-                               MessageBox( NULL, ( LPCTSTR)ErrorInfo,           \
-								   TEXT("GetLastError"), MB_OK|MB_ICONINFORMATION );     \
-                               LocalFree( lpMsgBuf );}
+                                LPVOID   lpMsgBuf;                               \
+                                FormatMessage(                                   \
+                                    FORMAT_MESSAGE_ALLOCATE_BUFFER               \
+                                        | FORMAT_MESSAGE_FROM_SYSTEM,            \
+                                    NULL, GetLastError(),                        \
+                                    MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),   \
+                                    (LPTSTR) &lpMsgBuf, 0, NULL);                \
+                                sprintf_s(ErrorInfo, MAX_SYS_ERROR_MSG_LEN, "Error at File:%s, Line:%d, %s",__FILE__, __LINE__, (char *)lpMsgBuf);\
+                                MessageBox( NULL, ( LPCTSTR)ErrorInfo,           \
+                                    TEXT("GetLastError"), MB_OK|MB_ICONINFORMATION );     \
+                                LocalFree( lpMsgBuf );}
 //---------------------------------------------------------------------------
 
 #define DEBUG_PRINT(fmt, ...) DebugTrace::getInstance()->m_pfDebugPrint((fmt), ##__VA_ARGS__)
@@ -39,37 +39,37 @@ typedef void (*pStartSaveToFile)(char *pFileName);
 
 enum RUNNING_BIT_SIZE
 {
-	RBS_x86,
-	RBS_x64,
+    RBS_x86,
+    RBS_x64,
 
-	RBS_BUTT
+    RBS_BUTT
 };
 
 class DebugTrace
 {
 private:
-	HINSTANCE   hDebugDllInst;
-	HWND 		m_hDebugForm;
+    HINSTANCE   hDebugDllInst;
+    HWND         m_hDebugForm;
 
-	enum RUNNING_BIT_SIZE getRunningBitSize();
-	DebugTrace();
+    enum RUNNING_BIT_SIZE getRunningBitSize();
+    DebugTrace();
     bool Initial(const char *pTitle);
 public:
-	pCreateFunc m_pfCreateDebugForm;
-	pFunc m_pfDestoryDebugForm;
-	pFunc m_pfCloseDebugForm;
-	pFunc m_pfShowDebugForm;
-	pFunc m_pfHideDebugForm;
-	pDebugPrintFunc m_pfDebugPrint;
+    pCreateFunc m_pfCreateDebugForm;
+    pFunc m_pfDestroyDebugForm;
+    pFunc m_pfCloseDebugForm;
+    pFunc m_pfShowDebugForm;
+    pFunc m_pfHideDebugForm;
+    pDebugPrintFunc m_pfDebugPrint;
 
-	pSetColorSize m_pfSetColorSize;
-	pGetColorSize m_pfGetColorSize;
-	pOutPutMem m_pfOutPutMem;
+    pSetColorSize m_pfSetColorSize;
+    pGetColorSize m_pfGetColorSize;
+    pOutPutMem m_pfOutPutMem;
 
-	pPrintToBufferFunc m_pPrintToBuffer;
-	pSetPrintSpeed m_pfSetPrintSpeed;
-	pOutPutMemToMem m_pfOutPutMemToBuffer;
-	pStartSaveToFile m_pfStartSaveToFile;
+    pPrintToBufferFunc m_pPrintToBuffer;
+    pSetPrintSpeed m_pfSetPrintSpeed;
+    pOutPutMemToMem m_pfOutPutMemToBuffer;
+    pStartSaveToFile m_pfStartSaveToFile;
     pFunc m_pfStopSaveToFile;
 
     ~DebugTrace();
